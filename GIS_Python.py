f1a82e649b076c9459ebc531f94eb728959e6c41
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2023-04-24 17:21:58 -0500

    Update GIS_Python.py

--- GIS_Python.py
+++ GIS_Python.py
@@ -20,4 +20,45 @@ df = df.rename(columns={'B': 'B_orig', 'B_y': 'C'})
 
 print(df)
 
+-------------------------#Extraxt value to points
+#import required libraries
+%matplotlib inline
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import rasterio
+from rasterio.plot import show
+import pandas as pd
+
+#open point shapefile
+pointData = gpd.read_file(r'C:\Users\arasht\OneDrive\0-TAMU\AAL Flood\Geopandas\extractPointDatafromRasterFile\Shp\pointData.shp')
+print(pointData.crs)
+pointData.plot()
+
+#open raster file
+ndviRaster = rasterio.open(r'C:\Users\arasht\OneDrive\0-TAMU\AAL Flood\Geopandas\extractPointDatafromRasterFile\Rst\ndviImage.tiff')
+print(ndviRaster.crs)
+#count number of bands
+print(ndviRaster.count)
+
+#show point and raster on a matplotlib plot
+fig, ax = plt.subplots(figsize=(12,12))
+pointData.plot(ax=ax, color='orangered')
+show(ndviRaster, ax=ax)
+
+#extract xy from point geometry
+for point in pointData['geometry']:
+    print(point.xy[0][0],point.xy[1][0])
+
+#extract point value from raster
+#put the results in a data frame
+rows_list = []
+for point in pointData['geometry']:
+    x = point.xy[0][0]
+    y = point.xy[1][0]
+    row, col = ndviRaster.index(x,y)
+    ndvi_value = ndviRaster.read(1)[row,col]
+    rows_list.append({'row': row, 'col': col, 'ndvi_value': ndvi_value})
+
+df = pd.DataFrame(rows_list)
+
 -------------------------#
